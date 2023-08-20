900c1a0c39245653dde2c7f5877b6f224d35c557
Linko-3D <dbekhouc@gmail.com>
2023-08-20 14:41:50 +0200

    tweak

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
 });
-
-
-
-const icons = document.querySelectorAll('.icon-select');
-const departmentsSelect = document.getElementById('departments');
-const searchContainer = document.getElementById('searchContainer');
-
-icons.forEach(icon => {
-    icon.addEventListener('click', () => {
-        icons.forEach(icon => {
-            icon.querySelector('.padding-highlight').classList.remove('active');
-        });
-        icon.querySelector('.padding-highlight').classList.add('active');
-
-        // Toggle between select and input search
-        if (departmentsSelect.style.display !== 'none') {
-            departmentsSelect.style.display = 'none';
-            searchContainer.style.display = 'block';
-        } else {
-            departmentsSelect.style.display = 'block';
-            searchContainer.style.display = 'none';
-        }
-    });
-});
-

94ed399cfac3a760eb87e6baa94ecb8af5fa62a9
Linko-3D <dbekhouc@gmail.com>
2023-09-07 14:54:40 +0200

    tweak

--- app.js
+++ app.js
@@ -17,29 +17,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Burger-menu ############################################################################################
 
-  // Get references to the icon and menu elements
-  const menuIcon = document.getElementById('menu-icon');
-  const menu = document.getElementById('menu');
+  // // Get references to the icon and menu elements
+  // const menuIcon = document.getElementById('menu-icon');
+  // const menu = document.getElementById('menu');
 
-  // Add a click event listener to the icon
-  menuIcon.addEventListener('click', function() {
-      // Toggle the 'active' class on the icon
-      menuIcon.classList.toggle('active');
+  // // Add a click event listener to the icon
+  // menuIcon.addEventListener('click', function() {
+  //     // Toggle the 'active' class on the icon
+  //     menuIcon.classList.toggle('active');
 
-      // Toggle the 'active' class on the menu
-      menu.classList.toggle('active');
+  //     // Toggle the 'active' class on the menu
+  //     menu.classList.toggle('active');
 
-      // Check if the icon has the 'active' class and update the 'name' attribute accordingly
-      if (menuIcon.classList.contains('active')) {
-          menuIcon.setAttribute('name', 'close-outline');
-          // Disable scrolling when the menu is active
-          document.body.style.overflow = 'hidden';
-      } else {
-          menuIcon.setAttribute('name', 'menu-outline');
-          // Enable scrolling when the menu is not active
-          document.body.style.overflow = 'auto';
-      }
-  });
+  //     // Check if the icon has the 'active' class and update the 'name' attribute accordingly
+  //     if (menuIcon.classList.contains('active')) {
+  //         menuIcon.setAttribute('name', 'close-outline');
+  //         // Disable scrolling when the menu is active
+  //         document.body.style.overflow = 'hidden';
+  //     } else {
+  //         menuIcon.setAttribute('name', 'menu-outline');
+  //         // Enable scrolling when the menu is not active
+  //         document.body.style.overflow = 'auto';
+  //     }
+  // });
 
 
   // Toggle options ##########################################################
